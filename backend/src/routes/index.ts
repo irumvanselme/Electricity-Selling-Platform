842c1c02ef7c902d12b4f49fcbe8f95956ea0dc2
irumvanselme <andesanselme@gmail.com>
2022-02-17 11:26:01 +0200

    intitial commit

---
+++ backend/src/routes/index.ts
@@ -0,0 +1,19 @@
+import { Express, Request, Response } from "express";
+import { createPostHandler, getPostsHandler } from "./posts";
+import { createUserHandler, getUserHandler, getUsersHandler } from "./users";
+
+export default function routes(app: Express) {
+	app.get("/healthcheck", (req: Request, res: Response) =>
+		res.sendStatus(200)
+	);
+
+	app.get("/api/users", (req, res) => getUsersHandler(req, res));
+
+	app.get("/api/users/:id", (req, res) => getUserHandler(req, res));
+
+	app.post("/api/users", (req, res) => createUserHandler(req, res));
+
+	app.get("/api/posts", (req, res) => getPostsHandler(req, res));
+
+	app.post("/api/posts", (req, res) => createPostHandler(req, res));
+}
